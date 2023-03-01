920c22171be6e27770ac6105853308b9b59648af
LucasRDelfino <lucasrdelfino2005@gmail.com>
2023-02-28 22:31:11 -0300

    Primeira aula python fiap

---
+++ main.py
@@ -0,0 +1,72 @@
+# EX 1
+print ("Hello World")
+
+
+# EX 2
+p = "Hello"
+p2 = " World"
+
+print(p,p2)
+
+
+# EX 3
+primeira_palavra = "Hello"
+primeira_palavra += " World"
+
+print(primeira_palavra)
+
+
+primeira_palavra = "Hello"
+primeira_palavra = primeira_palavra + " World"
+
+print (primeira_palavra)
+
+#EX 4
+
+var1 = "Meu"
+var1 = var1 + " nome"
+var1 = var1 + " é"
+var1 = var1 + " lucas"
+
+print(var1)
+
+#EX 5
+var1 = "Eu"
+var1 = var1 + " sou"
+var1 = var1 + " aluno"
+var1 = var1 + " de"
+var1 = var1 + " engenharia"
+var1 = var1 + " de"
+var1 = var1 + " software"
+
+print(var1)
+
+
+#EX6
+var1 = 0
+var1 = var1 + 10
+var1 = var1 + 5
+var1 = var1 + 7
+var1 = var1 + 6
+var1 = var1 + 3
+
+print(f"O total da compra é igual a : R${var1}")
+
+
+#EX7
+var1 = input("diga oi : ")
+
+print(var1)
+
+
+#EX8
+nome = input("Digite seu nome :")
+
+print(f"Seu nome é: {nome}")
+
+
+#EX9
+num1 = float(input("Digite o primeiro número"))
+num2 = float(input("Digite o segundo número"))
+
+print(f"A soma dos números é igual a {num1+num2} e a multiplicação é igual a {num1*num2}")
