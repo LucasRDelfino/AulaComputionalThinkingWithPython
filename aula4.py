ce9345d71e831c729ab666164bce21660075b5fa
Lucas Rodrigues Delfino <89103771+LucasRDelfino@users.noreply.github.com>
2023-03-21 22:50:03 -0300

    Update aula4.py
    Adding class 4

--- aula4.py
+++ aula4.py
@@ -1,18 +1,18 @@
 #EX1
 
-# n1 = int(input("Digite o primeiro numero"))
-# n2 = int(input("Digite o segundo numero"))
-# n3 = int (input ("Digite o terceiro numero"))
-# n4 = int (input ("Digite o quarto numero"))
+n1 = int(input("Digite o primeiro numero"))
+n2 = int(input("Digite o segundo numero"))
+n3 = int (input ("Digite o terceiro numero"))
+n4 = int (input ("Digite o quarto numero"))
 
-# if n1>n2 and n1>n3 and n1>n4: 
-#     print(f"O maior numero é {n1}")
-# elif n2>n3 and n2>n4 :
-#     print (f"O maior numero é {n2}")
-# elif n3>n4 :
-#     print (f"O maior numero é {n3}")
-# else :
-#     print (f"O maior numero é {n4}")
+if n1>n2 and n1>n3 and n1>n4: 
+    print(f"O maior numero é {n1}")
+elif n2>n3 and n2>n4 :
+    print (f"O maior numero é {n2}")
+elif n3>n4 :
+    print (f"O maior numero é {n3}")
+else :
+    print (f"O maior numero é {n4}")
 
 
 #EX2 
@@ -45,3 +45,24 @@ else :
     else :
         print (f"O menor numero é {n4}")
 
+
+#EX3
+
+n = int(input("Digite o numero de amaciantes que você deseja comprar"))
+cadastro = input("Você possui cadastro na loja?")
+
+
+if n>100 and cadastro=="sim" :
+    print(f"O total da sua compra é igual a {n*0.30}")
+elif n>100 or cadastro=="sim" :
+    print(f"O total da sua compra é igual a {n*0.35}")
+else :
+    print(f"O total da sua compra é igual a {n*0.50}")
+
+#while
+i = 1
+soma = 0
+while i < 11 :
+    soma += i 
+    i+=1
+    print(soma)
