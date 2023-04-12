aa3225a6f6db902cbf9a35a8d86ec1ecbc2c51e4
Lucas Rodrigues Delfino <89103771+LucasRDelfino@users.noreply.github.com>
2023-04-11 22:51:13 -0300

    adding the list of exercises

---
+++ excercise_list.py
@@ -0,0 +1,23 @@
+#EX1
+# n1 = int(input("Digite um número entre 0 e 10"))
+
+
+# while not (n1>=0 and n1<11) :
+#     n1 = int(input("Burrão , Digite um número entre 0 e 10"))
+
+
+# print (f"Você digitou o número {n1}") 
+
+
+#EX2
+
+# nome = input ("Digite seu nome : ")
+
+# while len(nome)<3 :
+#     nome = input ("Digite um nome com mais de três caracteres : ")
+#     print(f"Seu nome é {nome}")
+
+# idade = input ("Digite sua idade")
+
+
+
--- verifications.py
+++ verifications.py
@@ -16,38 +16,40 @@ while resposta!="s" and resposta!="n" :
     resposta = input ("Você digitou errado , digite s/n")
 
 if resposta=="s" : 
-    n1 = input ("Digite um número")
-    n2 = input ("Digite outro número")
-
-    while not n1.isnumeric() or not n2.isnumeric() :
-        print ("Você digitou algum valor errado digite apenas numeros")
+    
+    while True :
         n1 = input ("Digite um número")
         n2 = input ("Digite outro número")
 
+        while not n1.isnumeric() or not n2.isnumeric() :
+            print ("Você digitou algum valor errado digite apenas numeros")
+            n1 = input ("Digite um número")
+            n2 = input ("Digite outro número")
 
-    operacao = input ("Digite a operacao desejada na calculadora 1.Adição 2.Subtração 3.Multiplicação 4.Divisão 5.Sair")
+        operacao = input ("Digite a operacao desejada na calculadora 1.Adição 2.Subtração 3.Multiplicação 4.Divisão 5.Sair")
 
-    while operacao!="1" and operacao!="2" and operacao!="3" and operacao!="4" and operacao!="5" :
-        operacao = input ("Você digitou alguma coisa errada , escolha entre 1.Adição 2.Subtração 3.Multiplicação 4.Divisão 5.Sair")
+        while not (operacao=="1" or operacao=="2" or operacao=="3" or operacao=="4" or operacao=="5") :
+            operacao = input ("Você digitou alguma coisa errada , escolha entre 1.Adição 2.Subtração 3.Multiplicação 4.Divisão 5.Sair")
 
-    n1 = float(n1)
-    n2 = float(n2)
-    operacao = int(operacao)
+        n1 = float(n1)
+        n2 = float(n2)
+        operacao = int(operacao)
 
-    if operacao==1 :
-            resultado = n1+n2
-            print(f"O resultado da soma é igual a {resultado}")
-    elif operacao==2 : 
-            resultado = n1-n2
-            print(f"O resultado da subtracao é igual a {resultado}")
-    elif operacao==3 :
-            resultado = n1*n2
-            print(f"O resultado da multiplicacao é igual a {resultado}")
-    elif operacao==4:
-            resultado = n1/n2
-            print(f"O resultado da divisao é igual a {resultado}")
-    else :
-        print("Você saiu do programa")
+        if operacao==1 :
+                resultado = n1+n2
+                print(f"O resultado da soma é igual a {resultado}")
+        elif operacao==2 : 
+                resultado = n1-n2
+                print(f"O resultado da subtracao é igual a {resultado}")
+        elif operacao==3 :
+                resultado = n1*n2
+                print(f"O resultado da multiplicacao é igual a {resultado}")
+        elif operacao==4:
+                resultado = n1/n2
+                print(f"O resultado da divisao é igual a {resultado}")
+        else :
+            print("Você saiu do programa")
+            break
 
 else : 
     print ("BLZ VACILÃO")
