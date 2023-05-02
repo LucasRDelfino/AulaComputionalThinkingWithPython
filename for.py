be8c70c7c3b049c0121d5838426f6a13a7d6ec0a
Lucas Rodrigues Delfino <89103771+LucasRDelfino@users.noreply.github.com>
2023-05-02 20:56:36 -0300

    updating the class of lists and for

--- for.py
+++ for.py
@@ -1,33 +1,43 @@
-#EX1
-for i in range(100) :
-    if i%2==0:
-        print(f"Número {i} é par")
-    else :
-        print(f"Número {i} é impar")
+# #EX1
+# for i in range(100) :
+#     if i%2==0:
+#         print(f"Número {i} é par")
+#     else :
+#         print(f"Número {i} é impar")
 
-#EX2
-for num in range(1,11) : 
-    for i in range(1,11) :
-        print(f"{num}*{i}={i*num}")
+# #EX2
+# for num in range(1,11) : 
+#     for i in range(1,11) :
+#         print(f"{num}*{i}={i*num}")
 
-#EX3
-soma = 0
-for num in range(1,11):
-    soma += int(input("Digite o número desejado"))
+# #EX3
+# soma = 0
+# for num in range(1,11):
+#     soma += int(input("Digite o número desejado"))
 
 
-print (f"O valor da soma é {soma} e a média {soma/10}")
+# print (f"O valor da soma é {soma} e a média {soma/10}")
 
-#EX4
-usuario = "lusquinhas"
-senha = "lusquinhas123"
+# #EX4
+# usuario = "lusquinhas"
+# senha = "lusquinhas123"
 
-for i in range(3) :
-    user_name = input("Digite o usuario")
-    user_password = input("Digite a senha")
-    if usuario==user_name and senha==user_password :
-        print("Você entrou no sistema")
-        break
-    print("Senha inválida , tente novamente")
+# for i in range(3) :
+#     user_name = input("Digite o usuario")
+#     user_password = input("Digite a senha")
+#     if usuario==user_name and senha==user_password :
+#         print("Você entrou no sistema")
+#         break
+#     print("Senha inválida , tente novamente")
 
-print("Você atingiu o número de tentativas")
+# print("Você atingiu o número de tentativas")
+
+#EX5
+
+tamanho = int(input("Digite o tamanho da lista :"))
+list = []
+
+for i in range(tamanho):
+    num = int(input("Digite um numero para ser adicionado a lista"))
+    list.append(num)
+print(list)
--- list.py
+++ list.py
@@ -1,13 +1,41 @@
+#EX1
 # list = [i for i in range(1,11)] 
 
 # for i in range(len(list)):
 #     print(list[i])
 
+#EX2
+# professores = ["Eduardo","Israel","Danilo","Cordeiro","Ana"]
 
-professores = ["Eduardo","Israel","Danilo","Cordeiro","Ana"]
+# for i in range(len(professores)):
+#     if professores[i]=="Danilo":
+#         print(f"{professores[i]} é o melhor professor")
+#         break
+#     print(professores[i])
 
-for i in range(len(professores)):
-    if professores[i]=="Danilo":
-        print(f"{professores[i]} é o melhor professor")
+
+#EX3
+# frase = ""
+# list = ["dan","isra","cordeiro"]
+# indice = 0
+# for nomes in list :
+#     if indice < len(list)-2:
+#             frase += f"O {nomes}, "
+#     elif indice == len(list)-2:
+#             frase += f"{nomes} e "
+#     else :
+#         frase += f"{nomes} são professores"
+#     indice+=1
+
+# print(frase)
+
+#EX4
+nomes = ["dan","isra","cordeiro"]
+teste = input ("digite um nome :")
+
+for i in range(len(nomes)):
+    if nomes[i]== teste:
+        print(f"O nome está no indice {i} da lista")
         break
-    print(professores[i])
+    else:
+        print("Esse nome não está na lista")
