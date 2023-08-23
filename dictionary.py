1256d8763b941eaf7681d945ee77819e6bb5d3c3
Lucas Rodrigues Delfino <89103771+LucasRDelfino@users.noreply.github.com>
2023-08-22 22:50:09 -0300

    Class where finished counted of matriz and started counted of dictionarys

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+from functions import acha_maior
+
+parentes = {}
+parentes['pai'] = 'Edson'
+parentes['Mae'] = 'Adna'
+
+for key in parentes.keys():
+    print(f"O nome do/da seu/seu {key} é {parentes[key]}")
+
+
+modelos = {'carros' : ['jetta','celta','uno de escada'],
+           'precos'  : [1000 , 2000 , 3000]}
+
+print(f"O carro mais caro é o {acha_maior(modelos['precos'],modelos['carros'])}")
+maior = modelos['precos'][0]
+indice_maior = 0
+
+
--- matriz.py
+++ matriz.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+import random
 
 def diagonal(m):
     for i in range(len(m)):
@@ -14,13 +14,12 @@ def cria_matriz(l, c):
     for i in range(l):
         matriz.append([])
         for j in range(c):
-            matriz[i].append(0)
+            matriz[i].append(random.randint(0,10))
     return matriz
 def altera_valor_linhas_pares_matriz(m):
-    for i in range(len(m)):
+    for i in range(0,len(m),2):
         for j in range(len(m[i])):
-            if i%2==0:
-                m[i][j] = 1
+            m[i][j] = 1
     return m
 
 def altera_valor_colunas_impares(m):
@@ -29,25 +28,28 @@ def altera_valor_colunas_impares(m):
             m[i][j] = 2
     return m
 
+def altera_valor_colunas_pares(m):
+    for j in range(0,len(m),2):
+        for i in range(len(m)):
+            m[i][j] = 1
+    return m
+
 def soma_matrizes(m1,m2):
      cria_matriz(3,3)
      for i in range(len(m1)):
          for j in range(len(m1[0])):
             m = m1[i][j] + m2[i][j]
             matriz[i][j] = m
-
          return matriz
 
-
-def tabuleiro():
-    matriz = cria_matriz(8,8)
+def tabuleiro(matriz):
     for i in range(len(matriz)):
         for j in range(len(matriz[0])):
-            if i%2==0 and j%2==0:
+            if i%2==j%2:
                 matriz[i][j] = 0
             else :
                 matriz[i][j] = 1
-    mostra_matriz(matriz)
+    return matriz
 
 def transpoe_matriz(m):
     for i in range(len(m)):
@@ -57,11 +59,35 @@ def transpoe_matriz(m):
             m[j][i] = aux
 
         return mostra_matriz(m)
+def soma_lista(lista):
+    soma = 0
+    for elem in lista:
+        soma+=elem
+    return soma
+def multiplica_matriz():
+    alunos = 10
+    notas = cria_matriz(5,alunos)
+    pesos = [1,2,3,2,1]
+    soma_pesos = soma_lista(pesos)
+    medias = []
+    mostra_matriz(notas)
+    for j in range(alunos):
+        soma = 0
+        for i in range(len(pesos)):
+            soma += pesos[i]*notas[i][j]
+        media = soma/soma_pesos
+        medias.append(media)
+    print(medias)
 
-matriz = [[1,2,3],[4,5,6],[7,8,9]]
+matriz = cria_matriz(5,10)
+
+peso = [1,2,3,2,1]
+Soma = 0
+for i in range(len(peso)):
+  Soma += peso[i]*matriz[i][0]
+
 mostra_matriz(matriz)
-print()
-transpoe_matriz(matriz)
+print(Soma)
 
 
 
