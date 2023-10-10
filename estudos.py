9bc99125b1ce27c0313d1ecf53d1044ee4881748
Lucas Rodrigues Delfino <89103771+LucasRDelfino@users.noreply.github.com>
2023-10-10 20:46:05 -0300

    training try exception py

--- estudos.py
+++ estudos.py
@@ -1,7 +1,36 @@
-def acha_maior(valores,nomes) :
-    maior = lista[0]
-    for i in len(valores):
-        if valores[i]>maior:
-            indice = i
-            maior = valores[i]
-    return nomes[indice_maior]
+# def acha_maior(valores,nomes) :
+#     maior = lista[0]
+#     for i in len(valores):
+#         if valores[i]>maior:
+#             indice = i
+#             maior = valores[i]
+#     return nomes[indice_maior]
+
+# frase = "A aranha arranha a rã. A rã arranha a aranha. Nem a aranha arranha a rã. Nem a rã arranha a aranha."
+#
+# frase = frase.replace(".","")
+# frase = frase.lower()
+# frase = frase.split(" ")
+#
+# print(frase)
+# dic = {}
+#
+# for palavra in frase:
+#     if palavra not in dic.keys():
+#         dic[palavra] = 1
+#     else:
+#         dic[palavra] += 1
+#
+# print(dic)
+#
+
+lista= [1,2,3,4,5]
+
+maior = lista[0]
+
+for i in range(len(lista)):
+    if i > maior:
+        indice_maior = i
+        maior = lista[i]
+
+print(f"O maior número é {maior} e está no indice {i}")
--- whiskbar_dictionary.py
+++ whiskbar_dictionary.py
@@ -1,19 +1,19 @@
-'''frase = "A aranha arranha a rã. A rã arranha a aranha. Nem a aranha arranha a rã. Nem a rã arranha a aranha."
-frase = frase.replace(".",'')
-frase = frase.lower()
-print(frase)
-print("a" == "A")
-frase = frase.split(" ")
-print(frase)
+# frase = "A aranha arranha a rã. A rã arranha a aranha. Nem a aranha arranha a rã. Nem a rã arranha a aranha."
+# frase = frase.replace(".",'')
+# frase = frase.lower()
+# print(frase)
+# print("a" == "A")
+# frase = frase.split(" ")
+# print(frase)
+#
+# dic = {}
+# for palavra in frase:
+#     if palavra not in dic.keys():
+#         dic[palavra] = 1
+#     else:
+#         dic[palavra] += 1
+# print(dic)
 
-dic = {}
-for palavra in frase:
-    if palavra not in dic.keys():
-        dic[palavra] = 1
-    else:
-        dic[palavra] += 1
-print(dic)
-'''
 #pergunte ao usuário se ele é cliente ou funcionário
 #se for cliente, de as opções de vinhos da casa e pergunte seu endereço
 #pergunte informações a respeito de qual ele quer ver
@@ -133,3 +133,4 @@ else:
             print(pd.DataFrame(whiskeys))
         else:
             break
+
